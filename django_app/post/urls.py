c93593384a94968333c2579b369653180e2fdd78
yykdev <assavictory@gmail.com>
2017-06-13 10:47:42 +0900

    과제 진행 중

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<post_pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^(?P<post_pk>\d+)/modify/$', views.post_modify, name='post_modified'),
+    url(r'^create/$', views.post_create, name='post_create'),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,19 +1,68 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from member.models import User
 from .models import Post
 
-# Hello world!를 출력해주는 index 함수를 만든다
-def index(request):
-    context = {
 
-    }
-    # return render(request, 'post/index.html', context=context)
-    return HttpResponse('Hello world')
-
-# 모든 포스트 항목을 출력
 def post_list(request):
+    # 모든 Post목록을 'posts'라는 key로 context에 담아 return render처리
+    # post/post_list.html을 template으로 사용하도록 한다
+
+    # 각 포스트에 대해 최대 4개까지의 댓글을 보여주도록 템플릿에 설정
     posts = Post.objects.all()
     context = {
-        'posts' : posts
+        'posts': posts,
     }
-    return render(request, 'post/post_list.html', context=context)
+    return render(request, 'post/post_list.html', context)
+
+
+def post_detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'post/post_detail.html', context)
+
+
+def post_create(request):
+    # POST요청을 받아 Post객체를 생성 후 post_list페이지로 redirect
+    if request.method == 'GET':
+        return render(request, 'post/post_create.html')
+    elif request.method == 'POST':
+        user = User.objects.first()
+        image = request.FILES['image']
+        post = Post.objects.create(author=user, photo=image)
+        return redirect(request, 'post/post_list.html')
+
+
+def post_modify(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+
+    pass
+
+
+def post_delete(request, post_pk):
+    # post_pk에 해당하는 Post에 대한 delete요청만을 받음
+    # 처리완료후에는 post_list페이지로 redirect
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def comment_create(request, post_pk):
+    # POST요청을 받아 Comment객체를 생성 후 post_detail페이지로 redirect
+    pass
+
+
+def comment_modify(request, post_pk):
+    # 수정
+    pass
+
+
+def comment_delete(request, post_pk, comment_pk):
+    # POST요청을 받아 Comment객체를 delete, 이후 post_detail페이지로 redirect
+    pass
